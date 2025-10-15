3ff3ff52b67b042872f4a18bceb283398f29d1cd
Srishti Srivastava <srishtisri224@gmail.com>
2025-10-15 19:12:13 +0530

    Added the next 5 sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,15 +7,16 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        crimson: [' "Crimson Text" ', 'serif'],     
-        mono: ['Roboto Mono', 'monospace'],     
+        crimson: ['"Crimson Text"', 'serif'],
+        mono: ['"Roboto Mono"', 'monospace'],
+        sans: ['"DM Sans"', 'sans-serif'],
       },
       screens: {
-        mob: '375px',  
-        tab: '800px',   
-        lap: '1280px', 
+        mob: '375px',   // mobile
+        tab: '800px',   // tablet
+        lap: '1280px',  // laptop/desktop
       },
     },
   },
   plugins: [],
-}
+};
