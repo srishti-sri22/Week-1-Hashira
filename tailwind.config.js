5e4618dc7201a36f1dd3f3ccf4ca25cc9881d511
Srishti Srivastava <srishtisri224@gmail.com>
2025-10-17 21:52:02 +0530

    added the responsiveness for the tablet version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,47 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx,html}"
+//   ],
+//   theme: {
+//     extend: {
+//       fontFamily: {
+//         crimson: ['"Crimson Text"', 'serif'],
+//         mono: ['"Roboto Mono"', 'monospace'],
+//         sans: ['"DM Sans"', 'sans-serif'],
+//         rethinks: ['"Rethink Sans"', 'sans-serif'],
+//         reddit: ['"Reddit Mono"', 'monospace'],
+//       },
+//       screens: {
+//         mob: '375px',   // mobile
+//         tab: '800px',   // tablet
+//         lap: '1280px',  // laptop/desktop
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx,html}"
+    "./**/*.{js,ts,jsx,tsx,html}"
   ],
   theme: {
+    screens: {
+      mob: "375px",      // up to 799px
+      tab:  "800px",  // 800px–1279px
+      lap: "1280px",     // 1280px and above
+    },
     extend: {
       fontFamily: {
         crimson: ['"Crimson Text"', 'serif'],
         mono: ['"Roboto Mono"', 'monospace'],
         sans: ['"DM Sans"', 'sans-serif'],
-      },
-      screens: {
-        mob: '375px',   // mobile
-        tab: '800px',   // tablet
-        lap: '1280px',  // laptop/desktop
+        rethinks: ['"Rethink Sans"', 'sans-serif'],
+        reddit: ['"Reddit Mono"', 'monospace'],
       },
     },
   },
